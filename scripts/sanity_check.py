0f0d1c8738169bdeaaeb7dfded117d090bc17478
wrongu <richard.d.lange.13@dartmouth.edu>
2024-08-02 16:59:59 -0400

    refactored sanity_check.py to use a subTest for each results file

--- scripts/sanity_check.py
+++ scripts/sanity_check.py
@@ -1,9 +1,21 @@
 from pathlib import Path
 import torch
 import unittest
+from functools import wraps
 
 
 LAM_KEY = f"after_training_model2_part2_stitching_{1.0:.3f}"
+
+
+def foreach_file(test_fn):
+    @wraps(test_fn)
+    def wrapper(self):
+        for filename in self.files:
+            with self.subTest(filename):
+                results = torch.load(filename)
+                test_fn(self, results)
+
+    return wrapper
 
 
 class TestSanityCheck(unittest.TestCase):
@@ -12,137 +24,136 @@ class TestSanityCheck(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        files = list(cls.WHERE.glob("results_resnet18_resnet34_*_lr1.00e-03.pth"))
-        cls.data = {file.name: torch.load(file) for file in files}
+        cls.files = list(cls.WHERE.glob("results_resnet18_resnet34_*_lr1.00e-03.pth"))
 
-    def test_init_loss_sensible(self):
-        for key, value in self.data.items():
-            self.assertLess(
-                value["init"]["losses"]["model1_loss"]["test_cross_entropy"],
-                1.0,
+    @foreach_file
+    def test_init_loss_sensible(self, results):
+        self.assertLess(
+            results["init"]["losses"]["model1_loss"]["test_cross_entropy"], 1.0
+        )
+        self.assertLess(
+            results["init"]["losses"]["model2_loss"]["test_cross_entropy"], 1.0
+        )
+        self.assertGreater(
+            results["init"]["losses"]["stitching_model_loss"]["test_cross_entropy"], 1.0
+        )
+
+    @foreach_file
+    def test_regression_loss_sensible(self, results):
+        self.assertLess(
+            results["after_regression"]["losses"]["stitching_model_loss"][
+                "test_cross_entropy"
+            ],
+            results["init"]["losses"]["stitching_model_loss"]["test_cross_entropy"],
+        )
+
+    @foreach_file
+    def test_learning_loss_sensible(self, results):
+        self.assertLess(
+            results["after_training"]["losses"]["stitching_model_loss"][
+                "test_cross_entropy"
+            ],
+            results["after_regression"]["losses"]["stitching_model_loss"][
+                "test_cross_entropy"
+            ],
+        )
+
+    @foreach_file
+    def test_m2_loss_sensible(self, results):
+        self.assertLess(
+            results[LAM_KEY]["losses"]["stitching_model_loss"]["test_cross_entropy"],
+            results["after_training"]["losses"]["stitching_model_loss"][
+                "test_cross_entropy"
+            ]
+            * 1.1,
+        )
+        self.assertGreater(
+            results[LAM_KEY]["losses"]["stitching_model_loss"]["test_delta_weights"],
+            0.0,
+        )
+
+    @foreach_file
+    def test_regression_does_not_affect_non_stitching_params(self, results):
+        state_dict_before = results["init"]["state_dict"]
+        state_dict_after = results["after_regression"]["state_dict"]
+        for key in state_dict_before.keys():
+            if "stitching_layer" in key:
+                continue
+            if not isinstance(state_dict_before[key], torch.Tensor):
+                continue
+            self.assertTrue(
+                torch.allclose(state_dict_before[key], state_dict_after[key]),
+                msg=f"Key: {key}",
             )
-            self.assertLess(
-                value["init"]["losses"]["model2_loss"]["test_cross_entropy"],
-                1.0,
-            )
-            self.assertGreater(
-                value["init"]["losses"]["stitching_model_loss"]["test_cross_entropy"],
-                1.0,
-            )
 
-    def test_regression_loss_sensible(self):
-        for key, value in self.data.items():
-            self.assertLess(
-                value["after_regression"]["losses"]["stitching_model_loss"][
-                    "test_cross_entropy"
-                ],
-                value["init"]["losses"]["stitching_model_loss"]["test_cross_entropy"],
+    @foreach_file
+    def test_regression_does_affect_stitching_params(self, results):
+        state_dict_before = results["init"]["state_dict"]
+        state_dict_after = results["after_regression"]["state_dict"]
+        for key in state_dict_before.keys():
+            if "stitching_layer" not in key:
+                continue
+            if not isinstance(state_dict_before[key], torch.Tensor):
+                continue
+            self.assertFalse(
+                torch.allclose(state_dict_before[key], state_dict_after[key]),
+                msg=f"Key: {key}",
             )
 
-    def test_learning_loss_sensible(self):
-        for key, value in self.data.items():
-            self.assertLess(
-                value["after_training"]["losses"]["stitching_model_loss"][
-                    "test_cross_entropy"
-                ],
-                value["after_regression"]["losses"]["stitching_model_loss"][
-                    "test_cross_entropy"
-                ],
+    @foreach_file
+    def test_stitching_layer_learning_does_not_affect_non_stitching_params(
+        self, results
+    ):
+        state_dict_before = results["after_regression"]["state_dict"]
+        state_dict_after = results["after_training"]["state_dict"]
+        for key in state_dict_before.keys():
+            if "stitching_layer" in key:
+                continue
+            if not isinstance(state_dict_before[key], torch.Tensor):
+                continue
+            self.assertTrue(
+                torch.allclose(state_dict_before[key], state_dict_after[key]),
+                msg=f"Key: {key}",
             )
 
-    def test_m2_loss_sensible(self):
-        for key, value in self.data.items():
-            self.assertLess(
-                value[LAM_KEY]["losses"]["stitching_model_loss"]["test_cross_entropy"],
-                value["after_training"]["losses"]["stitching_model_loss"][
-                    "test_cross_entropy"
-                ] * 1.05,
+    @foreach_file
+    def test_stitching_layer_learning_does_affect_stitching_params(self, results):
+        state_dict_before = results["after_regression"]["state_dict"]
+        state_dict_after = results["after_training"]["state_dict"]
+        for key in state_dict_before.keys():
+            if "stitching_layer" not in key:
+                continue
+            if not isinstance(state_dict_before[key], torch.Tensor):
+                continue
+            self.assertFalse(
+                torch.allclose(state_dict_before[key], state_dict_after[key]),
+                msg=f"Key: {key}",
             )
-            self.assertGreater(
-                value[LAM_KEY]["losses"]["stitching_model_loss"]["test_delta_weights"],
-                0.0,
+
+    @foreach_file
+    def test_m2_learning_does_not_affect_wrong_stuff(self, results):
+        state_dict_before = results["after_training"]["state_dict"]
+        state_dict_after = results[LAM_KEY]["state_dict"]
+        for key in state_dict_before.keys():
+            if "stitching_layer" in key or "part2_model2" in key:
+                continue
+            if not isinstance(state_dict_before[key], torch.Tensor):
+                continue
+            self.assertTrue(
+                torch.allclose(state_dict_before[key], state_dict_after[key]),
+                msg=f"Key: {key}",
             )
 
-    def test_regression_does_not_affect_non_stitching_params(self):
-        for key, value in self.data.items():
-            state_dict_before = value["init"]["state_dict"]
-            state_dict_after = value["after_regression"]["state_dict"]
-            for key in state_dict_before.keys():
-                if "stitching_layer" in key:
-                    continue
-                if not isinstance(state_dict_before[key], torch.Tensor):
-                    continue
-                self.assertTrue(
-                    torch.allclose(state_dict_before[key], state_dict_after[key]),
-                    msg=f"Key: {key}",
-                )
-
-    def test_regression_does_affect_stitching_params(self):
-        for key, value in self.data.items():
-            state_dict_before = value["init"]["state_dict"]
-            state_dict_after = value["after_regression"]["state_dict"]
-            for key in state_dict_before.keys():
-                if "stitching_layer" not in key:
-                    continue
-                if not isinstance(state_dict_before[key], torch.Tensor):
-                    continue
-                self.assertFalse(
-                    torch.allclose(state_dict_before[key], state_dict_after[key]),
-                    msg=f"Key: {key}",
-                )
-
-    def test_stitching_layer_learning_does_not_affect_non_stitching_params(self):
-        for key, value in self.data.items():
-            state_dict_before = value["after_regression"]["state_dict"]
-            state_dict_after = value["after_training"]["state_dict"]
-            for key in state_dict_before.keys():
-                if "stitching_layer" in key:
-                    continue
-                if not isinstance(state_dict_before[key], torch.Tensor):
-                    continue
-                self.assertTrue(
-                    torch.allclose(state_dict_before[key], state_dict_after[key]),
-                    msg=f"Key: {key}",
-                )
-
-    def test_stitching_layer_learning_does_affect_stitching_params(self):
-        for key, value in self.data.items():
-            state_dict_before = value["after_regression"]["state_dict"]
-            state_dict_after = value["after_training"]["state_dict"]
-            for key in state_dict_before.keys():
-                if "stitching_layer" not in key:
-                    continue
-                if not isinstance(state_dict_before[key], torch.Tensor):
-                    continue
-                self.assertFalse(
-                    torch.allclose(state_dict_before[key], state_dict_after[key]),
-                    msg=f"Key: {key}",
-                )
-
-    def test_m2_learning_does_not_affect_wrong_stuff(self):
-        for key, value in self.data.items():
-            state_dict_before = value["after_training"]["state_dict"]
-            state_dict_after = value[LAM_KEY]["state_dict"]
-            for key in state_dict_before.keys():
-                if "stitching_layer" in key or "part2_model2" in key:
-                    continue
-                if not isinstance(state_dict_before[key], torch.Tensor):
-                    continue
-                self.assertTrue(
-                    torch.allclose(state_dict_before[key], state_dict_after[key]),
-                    msg=f"Key: {key}",
-                )
-
-    def test_m2_learning_does_affect_stitching_params(self):
-        for key, value in self.data.items():
-            state_dict_before = value["after_regression"]["state_dict"]
-            state_dict_after = value["after_training"]["state_dict"]
-            for key in state_dict_before.keys():
-                if "stitching_layer" not in key or "part2_model2" not in key:
-                    continue
-                if not isinstance(state_dict_before[key], torch.Tensor):
-                    continue
-                self.assertFalse(
-                    torch.allclose(state_dict_before[key], state_dict_after[key]),
-                    msg=f"Key: {key}",
-                )
+    @foreach_file
+    def test_m2_learning_does_affect_stitching_params(self, results):
+        state_dict_before = results["after_regression"]["state_dict"]
+        state_dict_after = results["after_training"]["state_dict"]
+        for key in state_dict_before.keys():
+            if "stitching_layer" not in key or "part2_model2" not in key:
+                continue
+            if not isinstance(state_dict_before[key], torch.Tensor):
+                continue
+            self.assertFalse(
+                torch.allclose(state_dict_before[key], state_dict_after[key]),
+                msg=f"Key: {key}",
+            )
